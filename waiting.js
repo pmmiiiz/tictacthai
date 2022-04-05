f843209ac46c835a474f6e37245d8a302390168d
pmmiiiz <56476217+pmmiiiz@users.noreply.github.com>
2022-04-05 19:22:25 +0700

    waitingroom

---
+++ waiting.js
@@ -0,0 +1,51 @@
+const refuser = firebase.database().ref("users")
+
+refuser.on("value", data => {
+    data = data.val()
+
+    const currentuser = firebase.auth().currentUser
+    console.log(currentuser);
+    console.log(data[currentuser.uid]);
+    console.log(data)
+
+    const user = data[currentuser.uid]
+    console.log(user.username);
+
+    document.querySelector('#pic-profile img').src = user.profile_picture
+    document.querySelector('#nickname div').innerHTML = user.username
+
+})
+
+const btnjoin = document.querySelector('#join-x');
+btnjoin.addEventListener('click', ready);
+
+function ready() {
+    document.querySelector('#readyjoin').innerHTML = "พร้อมแล้ว!!!"
+    document.querySelector('#readyjoin').style.color = "#FB4D44"
+    document.querySelector('#join-x').disabled = true;
+    document.querySelector('#join-x').classList.add("disabled")
+    document.querySelector('#cancel-x').disabled = false;
+    document.querySelector('#cancel-x').classList.remove("disabledd")
+}
+
+const btncancel = document.querySelector('#cancel-x');
+btncancel.addEventListener('click', cancel);
+  
+function cancel() {
+    document.querySelector('#readyjoin').innerHTML = "รอก่อน..."
+    document.querySelector('#readyjoin').style.color = "#E7BD4F"
+    document.querySelector('#cancel-x').disabled = true;
+    document.querySelector('#cancel-x').classList.add("disabledd")
+    document.querySelector('#join-x').disabled = false;
+    document.querySelector('#join-x').classList.remove("disabled")
+}
+
+
+const btnstart = document.querySelector('#btn-startgame');
+btnstart.addEventListener('click', start);
+  
+function start() {
+    setTimeout(function(){
+        window.location.href = "./play.html"
+    }, 1000); 
+}
